9f69279d99745f20343311004d2efc76f4a3e319
Josh Karpel <josh.karpel@gmail.com>
2021-12-01 20:29:12 -0600

    Solve Day 01 (#5)

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,14 +1,30 @@
-fn part_1() -> i64 {
-    -1
+use num::Integer;
+use std::fs::read_to_string;
+use std::iter::Sum;
+
+fn part_1<N: Integer>(depths: &[N]) -> usize {
+    depths.array_windows().filter(|[a, b]| a < b).count()
 }
 
-fn part_2() -> i64 {
-    -1
+fn part_2<'n, N: Integer + Sum<&'n N>>(depths: &'n [N]) -> usize {
+    depths
+        .windows(3)
+        .map(|window| window.iter().sum())
+        .collect::<Vec<N>>()
+        .array_windows()
+        .filter(|[a, b]| a < b)
+        .count()
 }
 
 pub fn solve() {
-    println!("Part 1: {}", part_1());
-    println!("Part 2: {}", part_2());
+    let depths: Vec<usize> = read_to_string("data/day_01.txt")
+        .unwrap()
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect();
+
+    println!("Part 1: {}", part_1(&depths));
+    println!("Part 2: {}", part_2(&depths));
 }
 
 #[cfg(test)]
@@ -16,8 +32,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part_1_examples() {}
+    fn part_1_examples() {
+        let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(part_1(&depths), 7)
+    }
 
     #[test]
-    fn part_2_examples() {}
+    fn part_2_examples() {
+        let depths = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+        assert_eq!(part_2(&depths), 5)
+    }
 }
