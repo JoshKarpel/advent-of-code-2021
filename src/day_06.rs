96e91c3bd56fda4416818c57c494ae70ae53e2af
Josh Karpel <josh.karpel@gmail.com>
2021-12-05 23:18:43 -0600

    need more clever

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,25 +1,54 @@
 use crate::utils::SolverResult;
+use itertools::Itertools;
+use std::fs::read_to_string;
 
-fn part_1() -> isize {
-    -1
+fn day(fish: &[u8]) -> Vec<u8> {
+    fish.iter().fold(Vec::new(), |mut new_fish, f| {
+        match f {
+            0 => {
+                new_fish.push(6);
+                new_fish.push(8);
+            }
+            fish => new_fish.push(fish - 1),
+        }
+        new_fish
+    })
 }
 
-fn part_2() -> isize {
-    -1
+fn part_1(fish: &[u8]) -> usize {
+    (0..80).fold(fish.to_vec(), |fish, _f| day(&fish)).len()
+}
+
+fn part_2(fish: &[u8]) -> usize {
+    (0..256).fold(fish.to_vec(), |fish, _f| day(&fish)).len()
 }
 
 pub fn solve() -> SolverResult {
-    println!("Part 1: {}", part_1());
-    println!("Part 2: {}", part_2());
+    let fish: Vec<u8> = read_to_string("data/day_06.txt")?
+        .trim()
+        .split(',')
+        .map(&str::parse)
+        .try_collect()?;
+
+    println!("Part 1: {}", part_1(&fish));
+    println!("Part 2: {}", part_2(&fish));
 
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn part_1_examples() {}
+    use crate::day_06::part_1;
+
+    const FISH: [u8; 5] = [3, 4, 3, 1, 2];
 
     #[test]
-    fn part_2_examples() {}
+    fn part_1_examples() {
+        assert_eq!(part_1(&FISH), 5934);
+    }
+
+    #[test]
+    fn part_2_examples() {
+        assert_eq!(part_1(&FISH), 26984457539);
+    }
 }
