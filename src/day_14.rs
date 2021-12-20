b61afe2d5b77035db6a190db12970dec2837eefa
Josh Karpel <josh.karpel@gmail.com>
2021-12-20 16:20:23 -0500

    track the pairs!

--- src/day_14.rs
+++ src/day_14.rs
@@ -4,43 +4,44 @@ use itertools::MinMaxResult::MinMax;
 use std::collections::HashMap;
 use std::fs::read_to_string;
 
+type Pair = [char; 2];
 type Polymer = Vec<char>;
 type Rules = HashMap<[char; 2], char>;
 
-fn grow(polymer: Polymer, rules: &Rules) -> Polymer {
-    polymer
-        .as_slice()
-        .array_windows()
-        .fold(vec![*polymer.first().unwrap()], |mut p, pair| {
-            if let Some(&insert) = rules.get(pair) {
-                p.push(insert);
-                p.push(pair[1]);
-            } else {
-                p.push(pair[1]);
-            }
+fn grow(polymer: &Polymer, rules: &Rules, steps: usize) -> usize {
+    let pair_counts = (0..steps).fold(
+        polymer.as_slice().array_windows().cloned().counts(),
+        |counts, step| {
+            println!("{} {:?}", step, counts);
+            let mut new_counts = HashMap::new();
+            counts.iter().for_each(|(pair, count)| {
+                if let Some(&insert) = rules.get(pair) {
+                    let left = new_counts.entry([pair[0], insert]).or_insert(0);
+                    *left += count;
 
-            p
-        })
-}
+                    let right = new_counts.entry([insert, pair[1]]).or_insert(0);
+                    *right += count;
+                } else {
+                    let orig = new_counts.entry(*pair).or_insert(0);
+                    *orig += count;
+                }
+            });
+            new_counts
+        },
+    );
 
-fn part_1(polymer: &Polymer, rules: &Rules) -> usize {
-    let final_polymer: Polymer =
-        (0..10).fold(polymer.clone(), |polymer, _step| grow(polymer, rules));
-
-    if let MinMax(min, max) = final_polymer.iter().counts().values().minmax() {
-        max - min
-    } else {
-        0
-    }
-}
-
-fn part_2(polymer: &Polymer, rules: &Rules) -> usize {
-    let final_polymer: Polymer = (0..40).fold(polymer.clone(), |polymer, step| {
-        println!("{} {}", step, polymer.len());
-        grow(polymer, rules)
+    // Now count the left character in each pair.
+    // The right character is the left character of some other pair, so it will be counted then...
+    let mut char_counts = HashMap::new();
+    pair_counts.iter().for_each(|(pair, count)| {
+        let left = char_counts.entry(pair[0]).or_insert(0);
+        *left += count;
     });
+    // ... except for the very last character, which is not the left character in any pair.
+    let last = char_counts.entry(*polymer.last().unwrap()).or_insert(0);
+    *last += 1;
 
-    if let MinMax(min, max) = final_polymer.iter().counts().values().minmax() {
+    if let MinMax(min, max) = char_counts.values().minmax() {
         max - min
     } else {
         0
@@ -70,8 +71,8 @@ fn parse_input(input: &str) -> (Polymer, Rules) {
 pub fn solve() -> SolverResult {
     let (polymer, rules) = parse_input(&read_to_string("data/day_14.txt")?);
 
-    println!("Part 1: {}", part_1(&polymer, &rules));
-    println!("Part 2: {}", part_2(&polymer, &rules));
+    println!("Part 1: {}", grow(&polymer, &rules, 10));
+    println!("Part 2: {}", grow(&polymer, &rules, 40));
 
     Ok(())
 }
@@ -103,9 +104,12 @@ CN -> C";
     #[test]
     fn part_1_examples() {
         let (polymer, rules) = parse_input(INPUT);
-        assert_eq!(part_1(&polymer, &rules), 1588);
+        assert_eq!(grow(&polymer, &rules, 10), 1588);
     }
 
     #[test]
-    fn part_2_examples() {}
+    fn part_2_examples() {
+        let (polymer, rules) = parse_input(INPUT);
+        assert_eq!(grow(&polymer, &rules, 40), 2188189693529);
+    }
 }
