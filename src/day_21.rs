dc91e0b6ba959bc5bd80c77998a2c21ca9553f8e
Josh Karpel <josh.karpel@gmail.com>
2021-12-23 11:29:43 -0500

    solve part 2

--- src/day_21.rs
+++ src/day_21.rs
@@ -1,13 +1,18 @@
 use crate::utils::SolverResult;
+use itertools::{iproduct, Itertools};
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
-fn part_1(positions: &Vec<usize>) -> usize {
-    let mut positions = positions.clone();
+type Positions = [usize; 2];
+type Scores = [usize; 2];
+
+fn part_1(positions: &Positions) -> usize {
+    let mut positions = *positions;
 
     let mut rolls = (1..=1000).cycle();
     let mut roll_count = 0;
 
-    let mut scores = vec![0; positions.len()];
+    let mut scores = [0, 0];
 
     'outer: loop {
         for (position, score) in positions.iter_mut().zip(scores.iter_mut()) {
@@ -25,15 +30,80 @@ fn part_1(positions: &Vec<usize>) -> usize {
     roll_count * scores.iter().min().unwrap()
 }
 
-fn part_2(_positions: &Vec<usize>) -> usize {
-    0
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+struct Game {
+    positions: Positions,
+    scores: Scores,
+    turn: usize,
 }
 
-fn parse_input(input: &str) -> Vec<usize> {
+lazy_static! {
+    // Precompute each possible distance and how many times it appears
+    static ref DISTANCES: HashMap<usize, usize> = iproduct!(1..=3, 1..=3, 1..=3)
+        .map(|rolls| rolls.0 + rolls.1 + rolls.2)
+        .counts();
+}
+
+type Winners = HashMap<Game, [usize; 2]>;
+
+fn play(game: Game, winners: &mut Winners) -> [usize; 2] {
+    if let Some(win_counts) = winners.get(&game) {
+        *win_counts
+    } else {
+        let mut win_counts = [0, 0];
+
+        // Recurse down into each new game
+        DISTANCES.iter().for_each(|(distance, count)| {
+            let position = (game.positions[game.turn] + distance) % 10;
+            let score = game.scores[game.turn] + if position == 0 { 10 } else { position }; // 0 is 10, otherwise they match
+
+            if score >= 21 {
+                win_counts[game.turn] += count;
+            } else {
+                let mut new_game = game.clone();
+
+                new_game.positions[game.turn] = position;
+                new_game.scores[game.turn] = score;
+                new_game.turn = match game.turn {
+                    0 => 1,
+                    1 => 0,
+                    _ => unreachable!(),
+                };
+
+                let new_wins = play(new_game, winners);
+                win_counts[0] += count * new_wins[0];
+                win_counts[1] += count * new_wins[1];
+            }
+        });
+
+        winners.insert(game, win_counts);
+
+        win_counts
+    }
+}
+
+fn part_2(positions: &Positions) -> usize {
+    let game = Game {
+        positions: *positions,
+        scores: [0, 0],
+        turn: 0,
+    };
+
+    // There are ~27^21 games to play, but only
+    // 10^2 * 21^2 * 2 game states (positions * scores * turns),
+    // so we'll have lots of hits in a game state -> win counts cache.
+    let mut winners: Winners = HashMap::new();
+
+    *play(game, &mut winners).iter().max().unwrap()
+}
+
+fn parse_input(input: &str) -> Positions {
     input
         .lines()
         .map(|line| line.chars().last().unwrap().to_digit(10).unwrap() as usize)
-        .collect()
+        .collect::<Vec<usize>>()
+        .try_into()
+        .unwrap()
 }
 
 pub fn solve() -> SolverResult {
