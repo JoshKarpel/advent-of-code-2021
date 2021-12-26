0937111b0b618ef0cd5ed8ffc1d2ce42a04fe267
Josh Karpel <josh.karpel@gmail.com>
2021-12-25 22:58:09 -0500

    solve part 2

--- src/day_22.rs
+++ src/day_22.rs
@@ -1,73 +1,84 @@
 use crate::utils::SolverResult;
-
-use itertools::iproduct;
+use itertools::Itertools;
 use regex::Regex;
-use std::collections::HashSet;
 use std::fs::read_to_string;
 
 #[derive(Debug, Copy, Clone)]
-enum Instruction {
-    On,
-    Off,
+struct Dim {
+    lower: isize,
+    upper: isize,
 }
 
-type Bounds = (isize, isize);
+impl Dim {
+    fn is_valid(&self) -> bool {
+        self.upper >= self.lower
+    }
+}
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 struct Cuboid {
-    x: Bounds,
-    y: Bounds,
-    z: Bounds,
+    dims: Vec<Dim>,
+    sign: isize,
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Step {
-    instruction: Instruction,
-    cuboid: Cuboid,
+impl Cuboid {
+    fn volume(&self) -> isize {
+        self.sign
+            * self
+                .dims
+                .iter()
+                .map(|d| 1 + d.upper - d.lower)
+                .product::<isize>()
+    }
+
+    fn intersection(&self, other: &Self) -> Option<Self> {
+        let c = Self {
+            dims: self
+                .dims
+                .iter()
+                .zip(other.dims.iter())
+                .map(|(s, o)| Dim {
+                    lower: s.lower.max(o.lower),
+                    upper: s.upper.min(o.upper),
+                })
+                .collect(),
+            sign: match (self.sign, other.sign) {
+                // This is the inclusion-exclusion rule, done iteratively.
+                // So by default, you flip the sign, because each intersection represents adding one more set to the intersection, which flips the sign of that term in the overall sum.
+                // But we also need to account for the "off" instructions
+                (1, 1) => -1,
+                (1, -1) => 1,
+                (-1, 1) => -1,
+                (-1, -1) => 1,
+                _ => unreachable!(),
+            },
+        };
+
+        (c.dims.iter().all(|d| d.is_valid())).then_some(c)
+    }
 }
 
-type Grid = HashSet<(isize, isize, isize)>;
-
-fn part_1(steps: &[Step]) -> usize {
+fn count_on(steps: &[Cuboid], initialization: bool) -> isize {
     steps
         .iter()
-        .fold(Grid::new(), |mut grid, step| {
-            if [
-                step.cuboid.x.0,
-                step.cuboid.x.1,
-                step.cuboid.y.0,
-                step.cuboid.y.1,
-                step.cuboid.z.0,
-                step.cuboid.z.1,
-            ]
-            .iter()
-            .any(|x| x.abs() > 50)
-            {
-                grid
-            } else {
-                let iter = iproduct!(
-                    step.cuboid.x.0..=step.cuboid.x.1,
-                    step.cuboid.y.0..=step.cuboid.y.1,
-                    step.cuboid.z.0..=step.cuboid.z.1
-                );
+        .filter(|step| !initialization || step.dims.iter().all(|d| d.upper <= 50 && d.lower >= -50))
+        .fold(Vec::new(), |mut cuboids: Vec<Cuboid>, step| {
+            let mut intersections = cuboids
+                .iter()
+                .filter_map(|cuboid| step.intersection(cuboid))
+                .collect_vec();
 
-                iter.for_each(|p| match step.instruction {
-                    Instruction::On => {
-                        grid.insert(p);
-                    }
-                    Instruction::Off => {
-                        grid.remove(&p);
-                    }
-                });
+            cuboids.append(&mut intersections);
 
-                grid
+            if step.sign == 1 {
+                cuboids.push(step.clone());
             }
-        })
-        .len()
-}
 
-fn part_2(_steps: &[Step]) -> usize {
-    0
+            cuboids
+        })
+        .iter()
+        .map(|cuboid| cuboid.volume())
+        .sum()
 }
 
 lazy_static! {
@@ -75,12 +86,16 @@ lazy_static! {
         Regex::new(r"(on|off) x=(-?\d+..-?\d+),y=(-?\d+..-?\d+),z=(-?\d+..-?\d+)").unwrap();
 }
 
-fn parse_range(range: &str) -> Bounds {
+fn parse_range(range: &str) -> Dim {
     let (lower, upper) = range.split_once("..").unwrap();
-    (lower.parse().unwrap(), upper.parse().unwrap())
+
+    Dim {
+        lower: lower.parse().unwrap(),
+        upper: upper.parse().unwrap(),
+    }
 }
 
-fn parse_input(input: &str) -> Vec<Step> {
+fn parse_input(input: &str) -> Vec<Cuboid> {
     input
         .lines()
         .map(|line| {
@@ -90,13 +105,13 @@ fn parse_input(input: &str) -> Vec<Step> {
             let y = parse_range(&captures[3]);
             let z = parse_range(&captures[4]);
 
-            Step {
-                instruction: match &captures[1] {
-                    "on" => Instruction::On,
-                    "off" => Instruction::Off,
+            Cuboid {
+                dims: vec![x, y, z],
+                sign: match &captures[1] {
+                    "on" => 1,
+                    "off" => -1,
                     _ => unreachable!(),
                 },
-                cuboid: Cuboid { x, y, z },
             }
         })
         .collect()
@@ -105,8 +120,8 @@ fn parse_input(input: &str) -> Vec<Step> {
 pub fn solve() -> SolverResult {
     let steps = parse_input(&read_to_string("data/day_22.txt")?);
 
-    println!("Part 1: {}", part_1(&steps));
-    println!("Part 2: {}", part_2(&steps));
+    println!("Part 1: {}", count_on(&steps, true));
+    println!("Part 2: {}", count_on(&steps, false));
 
     Ok(())
 }
@@ -145,12 +160,80 @@ on x=-41..9,y=-7..43,z=-33..15
 on x=-54112..-39298,y=-85059..-49293,z=-27449..7877
 on x=967..23432,y=45373..81175,z=27513..53682";
 
+    const LARGER_INPUT: &str = "\
+on x=-5..47,y=-31..22,z=-19..33
+on x=-44..5,y=-27..21,z=-14..35
+on x=-49..-1,y=-11..42,z=-10..38
+on x=-20..34,y=-40..6,z=-44..1
+off x=26..39,y=40..50,z=-2..11
+on x=-41..5,y=-41..6,z=-36..8
+off x=-43..-33,y=-45..-28,z=7..25
+on x=-33..15,y=-32..19,z=-34..11
+off x=35..47,y=-46..-34,z=-11..5
+on x=-14..36,y=-6..44,z=-16..29
+on x=-57795..-6158,y=29564..72030,z=20435..90618
+on x=36731..105352,y=-21140..28532,z=16094..90401
+on x=30999..107136,y=-53464..15513,z=8553..71215
+on x=13528..83982,y=-99403..-27377,z=-24141..23996
+on x=-72682..-12347,y=18159..111354,z=7391..80950
+on x=-1060..80757,y=-65301..-20884,z=-103788..-16709
+on x=-83015..-9461,y=-72160..-8347,z=-81239..-26856
+on x=-52752..22273,y=-49450..9096,z=54442..119054
+on x=-29982..40483,y=-108474..-28371,z=-24328..38471
+on x=-4958..62750,y=40422..118853,z=-7672..65583
+on x=55694..108686,y=-43367..46958,z=-26781..48729
+on x=-98497..-18186,y=-63569..3412,z=1232..88485
+on x=-726..56291,y=-62629..13224,z=18033..85226
+on x=-110886..-34664,y=-81338..-8658,z=8914..63723
+on x=-55829..24974,y=-16897..54165,z=-121762..-28058
+on x=-65152..-11147,y=22489..91432,z=-58782..1780
+on x=-120100..-32970,y=-46592..27473,z=-11695..61039
+on x=-18631..37533,y=-124565..-50804,z=-35667..28308
+on x=-57817..18248,y=49321..117703,z=5745..55881
+on x=14781..98692,y=-1341..70827,z=15753..70151
+on x=-34419..55919,y=-19626..40991,z=39015..114138
+on x=-60785..11593,y=-56135..2999,z=-95368..-26915
+on x=-32178..58085,y=17647..101866,z=-91405..-8878
+on x=-53655..12091,y=50097..105568,z=-75335..-4862
+on x=-111166..-40997,y=-71714..2688,z=5609..50954
+on x=-16602..70118,y=-98693..-44401,z=5197..76897
+on x=16383..101554,y=4615..83635,z=-44907..18747
+off x=-95822..-15171,y=-19987..48940,z=10804..104439
+on x=-89813..-14614,y=16069..88491,z=-3297..45228
+on x=41075..99376,y=-20427..49978,z=-52012..13762
+on x=-21330..50085,y=-17944..62733,z=-112280..-30197
+on x=-16478..35915,y=36008..118594,z=-7885..47086
+off x=-98156..-27851,y=-49952..43171,z=-99005..-8456
+off x=2032..69770,y=-71013..4824,z=7471..94418
+on x=43670..120875,y=-42068..12382,z=-24787..38892
+off x=37514..111226,y=-45862..25743,z=-16714..54663
+off x=25699..97951,y=-30668..59918,z=-15349..69697
+off x=-44271..17935,y=-9516..60759,z=49131..112598
+on x=-61695..-5813,y=40978..94975,z=8655..80240
+off x=-101086..-9439,y=-7088..67543,z=33935..83858
+off x=18020..114017,y=-48931..32606,z=21474..89843
+off x=-77139..10506,y=-89994..-18797,z=-80..59318
+off x=8476..79288,y=-75520..11602,z=-96624..-24783
+on x=-47488..-1262,y=24338..100707,z=16292..72967
+off x=-84341..13987,y=2429..92914,z=-90671..-1318
+off x=-37810..49457,y=-71013..-7894,z=-105357..-13188
+off x=-27365..46395,y=31009..98017,z=15428..76570
+off x=-70369..-16548,y=22648..78696,z=-1892..86821
+on x=-53470..21291,y=-120233..-33476,z=-44150..38147
+off x=-93533..-4276,y=-16170..68771,z=-104985..-24507";
+
     #[test]
     fn part_1_examples() {
-        assert_eq!(part_1(&parse_input(SMALL_INPUT)), 39);
-        assert_eq!(part_1(&parse_input(LARGE_INPUT)), 590784);
+        assert_eq!(count_on(&parse_input(SMALL_INPUT), true), 39);
+        assert_eq!(count_on(&parse_input(LARGE_INPUT), true), 590784);
     }
 
     #[test]
-    fn part_2_examples() {}
+    fn part_2_examples() {
+        assert_eq!(count_on(&parse_input(LARGER_INPUT), true), 474140);
+        assert_eq!(
+            count_on(&parse_input(LARGER_INPUT), false),
+            2758514936282235
+        );
+    }
 }
